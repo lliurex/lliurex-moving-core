1556b30f27f31e2a61d91947a89372fead8aac9c
Lt-Henry <quiqueiii@gmail.com>
2019-03-06 10:11:22 +0100

    * load operation relies on xsession and no longer profile.d * replacing gnome from file names

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ import subprocess
 
 
 setup(  name             = "Lliurex-moving-core",
-        version          = "2.0",
+        version          = "2.5",
         author           = "Enrique Medina Gremaldos",
         author_email     = "quiqueiii@gmail.com",
         url              = "http://lliurex.net/home/",
         package_dir      = {'': 'src'},
         packages         = ['net','net.Lliurex','net.Lliurex.Classroom','net.Lliurex.Classroom.MovingProfiles'],
-        data_files       = [("/usr/bin/",["bin/llx-moving-cmd","bin/llx-moving-gnome"]),("/etc/profile.d/",["profile.d/llx-moving-load.sh"]),
-                                 ("/etc/systemd/logind.conf.d/",["logind.conf.d/llx-moving-gnome.conf"]),("/etc/xdg/autostart/",["llx-moving-gnome.desktop"]),
+        data_files       = [("/usr/bin/",["bin/llx-moving-cmd","bin/llx-moving-plasma"]),("/etc/X11/Xsession.d/",["Xsession.d/95lliurex-moving"]),
+                                 ("/etc/systemd/logind.conf.d/",["logind.conf.d/net.lliurex.moving-profiles.conf"]),("/etc/xdg/autostart/",["net.lliurex.moving-profiles.desktop"]),
                                  ("/usr/share/lliurex-moving-core/postactions/",["postactions/050-whiskermenu.sh","postactions/060-remove_libreoffice_lock.sh","postactions/065-fix_libreoffice_save_path","postactions/070-remove_firefox_lock.sh"])]
      )
 
