0b70db5fceab44d468503c58fb8d37b6d4c95299
Lt-Henry <quiqueiii@gmail.com>
2019-05-23 13:44:15 +0200

    fixed setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(  name             = "Lliurex-moving-core",
         packages         = ['net','net.Lliurex','net.Lliurex.Classroom','net.Lliurex.Classroom.MovingProfiles'],
         data_files       = [("/usr/bin/",["bin/llx-moving-cmd","bin/llx-moving-service"]),
                                  ("/etc/X11/Xsession.d/",["95lliurex-moving-load"]),
-                                 ("/etc/xdg/lliurex/desktop/plasma-workspace/shutdown",["95lliurex-moving-save"])
+                                 ("/etc/xdg/lliurex/desktop/plasma-workspace/shutdown",["95lliurex-moving-save"]),
                                  ("/etc/systemd/logind.conf.d/",["logind.conf.d/net.lliurex.moving-profiles.conf"]),
                                  ("/usr/share/lliurex-moving-core/postactions/",["postactions/050-whiskermenu.sh","postactions/060-remove_libreoffice_lock.sh","postactions/065-fix_libreoffice_save_path","postactions/070-remove_firefox_lock.sh"])]
      )
