bc23696075f94fe08873eac2733044c7fbba2901
cless <hectorgh@gmail.com>
2019-09-30 13:07:02 +0200

    added new file to setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(  name             = "Lliurex-moving-core",
                                  ("/etc/X11/Xsession.d/",["95lliurex-moving-load"]),
                                  ("/etc/xdg/lliurex/desktop/plasma-workspace/shutdown",["95lliurex-moving-save"]),
                                  ("/etc/systemd/logind.conf.d/",["logind.conf.d/net.lliurex.moving-profiles.conf"]),
-                                 ("/usr/share/lliurex-moving-core/postactions/",["postactions/050-whiskermenu.sh","postactions/060-remove_libreoffice_lock.sh","postactions/065-fix_libreoffice_save_path","postactions/070-remove_firefox_lock.sh"])]
+                                 ("/usr/share/lliurex-moving-core/postactions/",["postactions/050-fix-xbel","postactions/060-remove_libreoffice_lock.sh","postactions/065-fix_libreoffice_save_path","postactions/070-remove_firefox_lock.sh"])]
      )
 
 
