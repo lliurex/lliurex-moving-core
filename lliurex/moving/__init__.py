94a87f2a660c029c3646191cdba47edfd656fb2f
cless <hectorgh@gmail.com>
2023-10-03 09:26:16 +0200

    ignorig .Microsoft dir in .config

--- lliurex/moving/__init__.py
+++ lliurex/moving/__init__.py
@@ -26,7 +26,7 @@ class Client:
             ".config": [
                 "/user-dirs.*", "/google-chrome/*",
                 "/chromium/Singleton*", "/libreoffice/4/.lock", "/pulse",
-                "/onedrive", "/lliurex-onedrive-config"
+                "/onedrive", "/lliurex-onedrive-config","/Microsoft"
             ],
             ".mozilla":
             ["/firefox/*.default/storage", "/firefox/*.default/lock"],
